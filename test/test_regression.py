d28b25cab3dd2e485734a9c167d6a389ea1225d1
Shyam Saladi <smsaladi@gmail.com>
2020-05-24 19:31:44 -0700

    fix paths for pytest to run from basedir

--- test/test_regression.py
+++ test/test_regression.py
@@ -4,21 +4,26 @@ Regression tests for codonharmonizer
 """
 
 import io
+import os.path
 import subprocess
 
 import pandas as pd
 import Bio.SeqIO
 
+def path(x):
+    dirn = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(dirn, x)
+
 def test_reference():
     output = subprocess.check_output(["codonharmonizer",
         "--write_freqs",
-        "GCA_000011385.1_ASM1138v1_cds_from_genomic.fna",
+        path("GCA_000011385.1_ASM1138v1_cds_from_genomic.fna"),
     ])
     output = io.StringIO(output.decode('utf-8'))
     df_test = pd.read_csv(output)
     df_test.sort_values('codon', inplace=True)
 
-    df_expected = pd.read_csv("Gvio_Freq.csv")
+    df_expected = pd.read_csv(path("Gvio_Freq.csv"))
     df_expected.sort_values('codon', inplace=True)
 
     assert df_test.equals(df_expected)
@@ -26,13 +31,13 @@ def test_reference():
 
 def test_recode():
     output = subprocess.check_output(["codonharmonizer",
-        "--source", "Gvio_Freq.csv",
-        "--target", "Eco_MG1655_Freq.csv",
-        "example_gene.faa"
+        "--source", path("Gvio_Freq.csv"),
+        "--target", path("Eco_MG1655_Freq.csv"),
+        path("example_gene.faa")
     ])
     output = io.StringIO(output.decode('utf-8'))
     test_recode = Bio.SeqIO.read(output, "fasta")
 
-    expected_recode = Bio.SeqIO.read("example_gene.recode.faa", "fasta")
+    expected_recode = Bio.SeqIO.read(path("example_gene.recode.faa"), "fasta")
     assert expected_recode.seq == test_recode.seq
     return
